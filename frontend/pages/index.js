ad2bf5addac36dcbd3c9938b036211d6cd07cc0d
Denis Vasenkov <webvasenkov@gmail.com>
2022-06-02 19:52:29 +0300

    upd dockerfiles & nginx.conf

---
+++ frontend/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <title>Concert Hall</title>
+                <meta name='description' content='Concert Hall by Alex' />
+                <link rel='icon' href='/favicon.ico' />
+            </Head>
+            <h1>Concert Hall</h1>
+        </div>
+    );
+}

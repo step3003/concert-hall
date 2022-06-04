1544af73a5933bba6c7bec31e25f6c87de6e8319
Denis Vasenkov <webvasenkov@gmail.com>
2022-06-04 22:32:23 +0300

    added scss & tested

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
     reactStrictMode: true,
     experimental: {
         outputStandalone: true,
+    },
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+    },
+    images: {
+        domains: ['51.250.30.77'],
     },
 };
 

427624e68dfad5aa724a118b98005c735c0854d2
Denis Vasenkov <webvasenkov@gmail.com>
2022-07-17 16:09:14 +0300

    added skeleton for events & view settings

---
+++ frontend/app/HOC/withAuth.ts
@@ -0,0 +1,20 @@
+import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+
+const withAuth = (Page: NextPage) => (props: any) => {
+    if (typeof window !== 'undefined') {
+        const Router = useRouter();
+        const user = localStorage.getItem('user');
+
+        if (!user) {
+            Router.replace('/');
+            return null;
+        }
+
+        return <Page {...props} />;
+    }
+
+    return null;
+};
+
+export default withAuth;
--- frontend/app/hooks.ts
+++ frontend/app/hooks.ts
@@ -1,5 +1,16 @@
+import { useEffect, useState } from 'react';
 import { RootState, RootDispatch } from './store';
 import { TypedUseSelectorHook, useSelector, useDispatch } from 'react-redux';
 
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 export const useAppDispatch: () => RootDispatch = useDispatch;
+
+export const useSSR = () => {
+    const [isSSR, setIsSSR] = useState(true);
+
+    useEffect(() => {
+        setIsSSR(false);
+    }, []);
+
+    return isSSR;
+};
